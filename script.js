397da2c59305fdadb101f3fe50ec8e12969c3ceb
Sneider Pineda <sney120@hotmail.com>
2022-09-02 18:57:15 -0500

    cambios en js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-function myfunction(){
-    document.getElementById('demo').innerHTML = 'Párrafo cambiado!';
-}
-
-function myfunction2(){
-    let x = (10 + 2) * 2;
+function MyFunction(){
+    let x = 'Texto cambiado.';    
     document.getElementById('demo').innerHTML = x;
 }
 
-//alert('Hola');
+function myfunction2(){
+    let x = 20;
+    alert('tengo ' + x + ' años');
+}
 
 console.log(`línea 1 de la cadena de texto
 línea 2 de la cadena de texto`);
 
-let emes = 'm';
-console.log(emes.repeat(10));
-console.log(`${emes}` + `${'a'.repeat(10)}`);
+let cadena = 'ma';
+
+console.log(cadena.repeat(10));
+console.log(`${cadena.repeat(2)}` + ' me ama');
 
 let ul = document.createElement('ul');
 
@@ -22,11 +22,71 @@ let li1 = document.createElement('li');
 let li2 = document.createElement('li');
 ul.appendChild(li1);
 ul.appendChild(li2);
-li1.innerHTML = 'Item numero 1';
-li2.innerHTML = 'Item numero 2';
+li1.innerHTML = 'Items numero 1';
+li2.innerHTML = 'Items numero 2';
 
 function hide(e){
     e.target.style.visibility = 'hidden';
 }
 
 ul.addEventListener('click', hide, false);
+
+function tocelsius(fahrenheit){
+    return (5/9) * (fahrenheit-32);
+}
+
+function saludar(){
+    let saludo;
+    let time = new Date().getHours();
+    
+    if(time < 12){
+        saludo = 'Buenos días';
+    } else if(time < 18){
+        saludo = 'Buenas tardes';
+    } else {
+        saludo = 'Buenas noches';
+    }
+    document.getElementById('demo').innerHTML = saludo;    
+}
+
+function dia(){
+    let dia2;
+    switch(new Date().getDay()){
+        case 0:
+            dia2 = 'Domingo';
+            break;
+        case 1:
+            dia2 = 'Lunes';
+            break;
+        case 2:
+            dia2 = 'Martes';
+            break;
+        case 3:
+            dia2 = 'Miercoles';
+            break;
+        case 4:
+            dia2 = 'Jueves';
+            break;
+        case 5:
+            dia2 = 'Viernes';
+            break;
+        case 6:
+            dia2 = 'Sabado';
+            break;
+        default:
+            dia2 = 'Dia no valido';
+            break;
+    }
+    document.getElementById('demo').innerHTML = 'Hoy es ' + dia2;
+}
+
+function listar(){
+    let productos = ["Granos", "Verduras", "Lácteos", "Aseo", "Carnes", "Mecato"];
+    let texto = "";
+    let i;
+
+    for(i=0; i < productos.length; i++){
+        texto += productos[i] + '<br>';
+    }
+    document.getElementById('demo').innerHTML =texto;
+}
